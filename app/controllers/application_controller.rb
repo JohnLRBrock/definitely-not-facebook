7ffefd47bd1b334eca02386860c58dad6b4c7063
khanthulhu <johnlrbrock@gmail.com>
2017-03-25 14:54:57 -0400

    new migration to add devise to users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login
+
+  private
+    def require_login
+      redirect_to root_url unless logged_in? 
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'sessions#new'
   get  'login', to: 'sessions#new'
   resources :users
