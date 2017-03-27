9ad1d49fa526716e5573ffe967c2d5e357ab5de3
khanthulhu <johnlrbrock@gmail.com>
2017-03-27 09:12:34 -0400

    added controller and method in users for handling logins

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     root 'devise/sessions#new'
   end
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSLclass Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController available within this file, see http://guides.rubyonrails.org/routing.html
 end
