70166af319075415d22d92b497e5bff68b692aca
khanthulhu <johnlrbrock@gmail.com>
2017-03-30 16:20:48 -0400

    added gravatar_for user helper and added it to user show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,10 @@ module UsersHelper
     attribute = attribute.to_s.gsub(/_/, ' ').capitalize
     "<p><strong>#{attribute}:</strong> #{user_attribute}</p>".html_safe
   end
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
