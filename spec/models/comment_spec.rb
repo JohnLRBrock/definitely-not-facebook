687ff826dad97c61717246239c00f9f7d05e5739
khanthulhu <johnlrbrock@gmail.com>
2017-04-13 12:17:21 -0400

    test for commen creation

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe Comment, type: :model do
     User.first.comments.build(post_id: Post.first.id).save
   end
 
+  let(:valid_params) { {user_id: User.first.id, body: "body" } }
+
   context "building comments without requisite attributes" do
     it "requires a post_id" do
       parentless = Comment.new(user_id: User.first.id,
@@ -24,8 +26,17 @@ RSpec.describe Comment, type: :model do
 
     it "requires a body" do
       bodyless = Comment.new(user_id: User.first.id,
-                               post_id: Post.first.id)
+                             post_id: Post.first.id)
       expect(bodyless.valid?).to be false
+    end
+  end
+
+  context "successfully creating comments" do
+    it "creates a comment" do
+      num = Comment.count
+      expect(Post.first.comments.build(valid_params).save).to be true
+      expect(Post.first.comments.build(valid_params).save).to be true
+      expect(num + 2).to eq(Comment.count)
     end
   end
 end
