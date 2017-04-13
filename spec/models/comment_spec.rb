1f41e68b205ea8bb0a7a3a983e52bf1f484b12e6
khanthulhu <johnlrbrock@gmail.com>
2017-04-13 12:03:33 -0400

    style

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,23 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   before(:all) do
-    User.delete_all
-    Comment.delete_all
+    User.destroy_all
+    Comment.destroy_all
     User.create(email:'adam@example.com', password: 'foobar', password_confirmation: 'foobar')
     User.first.posts.build(body: "Hello World").save
     User.first.comments.build(post_id: Post.first.id).save
   end
+
   context "building comments without requisite attributes" do
     it "requires a post_id" do
       parentless = Comment.new(user_id: User.first.id,
                                body: "Hello World")
       expect(parentless.valid?).to be false
     end
+
     it "requires a user_id" do
      posterless = Comment.new(user_id: User.first.id,
                                body: "Hello World")
      expect(posterless.valid?).to be false
     end
+
     it "requires a body" do
       bodyless = Comment.new(user_id: User.first.id,
                                post_id: Post.first.id)
