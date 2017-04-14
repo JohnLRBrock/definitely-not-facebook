a44f19b4a4ab1781451f0cbf46bc82d51304e89b
khanthulhu <johnlrbrock@gmail.com>
2017-04-14 12:39:57 -0400

    user attribute tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,31 +4,56 @@ RSpec.describe User, type: :model do
   # reset database and populate it with adam and eve
   before(:all) do
     User.destroy_all
-    User.create!(name: 'adam', email:'adam@example.com',
+    @adam = User.create!(name: 'adam', email:'adam@example.com',
                            password: 'foobar', password_confirmation: 'foobar')
-    User.create!(name: 'eve', email:'eve@example.com',
+    @eve = User.create!(name: 'eve', email:'eve@example.com',
                            password: 'foobar', password_confirmation: 'foobar')
   end
   
   describe "attributes" do
+    it 'responds to #name' do
+      expect(@adam).to respond_to(:name)
+    end
+    it 'responds to #email' do
+      expect(@adam).to respond_to(:email)
+    end
+    it 'responds to #location' do
+      expect(@adam).to respond_to(:location)
+    end
+    it 'responds to #occupation' do
+      expect(@adam).to respond_to(:occupation)
+    end
+    it 'responds to #age' do
+      expect(@adam).to respond_to(:age)
+    end
+    it 'responds to #self_summary' do
+      expect(@adam).to respond_to(:self_summary)
+    end
+
     it "doesn't allow duplicate names or emails" do
-      adam_clone = User.new(name: 'adam', email:'adam@example.com')
-      expect(adam_clone.valid?).to be false
+      adam_clone = User.new(name: 'adam', email:'adam@example.com',
+                            password: 'foobar', password_confirmation: 'foobar')
+      expect(adam_clone.save).to be false
     end
     
     it "is case insensitive with name and emails" do
-      capital_clone = User.new(name: 'AdAm', email:'AdAm@eXaMpLe.cOm')
+      capital_clone = User.new(name: 'AdAm', email:'AdAm@eXaMpLe.cOm',
+                               password: 'foobar', password_confirmation: 'foobar')
       expect(capital_clone.save).to be false
     end
     
     it "requires the existence of name" do
-      nameless = User.new(email: 'nameless@example.com')
-      expect(nameless.valid?).to be false
+      skip 'TODO'
+      nameless = User.new(email: 'nameless@example.com',
+                          password: 'foobar', password_confirmation: 'foobar')
+      expect(nameless.save).to be false
     end
+
     
     it "requires the existence of email" do
-      emailless = User.new(name: 'emailless')
-      expect(emailless.valid?).to be false
+      emailless = User.new(name: 'emailless',
+                           password: 'foobar', password_confirmation: 'foobar')
+      expect(emailless.save).to be false
     end
   end
 end
