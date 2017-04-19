a24be1ed119f06b60f833b7c52b9618e84e7ba73
khanthulhu <johnlrbrock@gmail.com>
2017-04-19 08:22:47 -0400

    test spec rewrites

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,17 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  before(:all) do
-    create(:user)
-  end
-  context 'building a post without requisite attributes' do
-    it 'needs a user' do
-      no_user = Comment.new(body: 'Hello world')
-      expect(no_user.valid?).to be false
-    end
-    it 'needs a body' do
-      no_body = Comment.new(user_id: User.first.id)
-      expect(no_body.valid?).to be false
-    end
+  subject(:post) { build(:post) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :user_id }
+    it { is_expected.to validate_presence_of :body }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,27 +5,36 @@ RSpec.describe User, type: :model do
   subject(:user) { build(:user) }
 
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of :email }
     #it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   end
 
   describe 'relationships' do
-    it { is_expected.to have_many(:posts) }
-    it { is_expected.to have_many(:comments) }
-    it { is_expected.to have_many(:likes) }
-    it { is_expected.to have_many(:friendships) }
-    it { is_expected.to have_many(:friends) }
-    it { is_expected.to have_many(:friend_requests) }
+    it { is_expected.to have_many :posts }
+    it { is_expected.to have_many :comments }
+    it { is_expected.to have_many :likes }
+    it { is_expected.to have_many :friendships }
+    it { is_expected.to have_many :friends }
+    it { is_expected.to have_many :friend_requests }
   end
 
   describe 'attributes' do
-    it { is_expected.to respond_to (:name) }
-    it { is_expected.to respond_to (:email) }
-    it { is_expected.to respond_to (:location) }
-    it { is_expected.to respond_to (:occupation) }
-    it { is_expected.to respond_to (:age) }
-    it { is_expected.to respond_to (:self_summary) }
+    it { is_expected.to respond_to :name }
+    it { is_expected.to respond_to :email }
+    it { is_expected.to respond_to :location }
+    it { is_expected.to respond_to :occupation }
+    it { is_expected.to respond_to :age }
+    it { is_expected.to respond_to :self_summary }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many :posts }
+    it { is_expected.to have_many :comments }
+    it { is_expected.to have_many :likes }
+    it { is_expected.to have_many :friendships }
+    it { is_expected.to have_many :friends }
+    it { is_expected.to have_many :friend_requests }
   end
 
   describe 'devise' do
