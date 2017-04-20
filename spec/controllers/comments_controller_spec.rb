9c64b1e2fe9421ad42eea6fa1254e52b89f8682b
khanthulhu <johnlrbrock@gmail.com>
2017-04-20 11:43:38 -0400

    added tests to comments controller spec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-=begin
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  let(:user)          { create(:user) }
+  let(:new_post)      { create(:post) }
+  let(:comment_attrs) { 
+                        { body: 'body', post_id: new_post.id }
+                      }
+
+  before do
+    #request.env['devise.mapping'] = Devise.mappings[:user]
+    sign_in user
+  end
+
+  describe 'POST #create' do
+    it 'incremement number of posts by 1' do
+      expect do
+        post :create, params: { comment: comment_attrs }
+      end.to change(Post, :count).by(1)
     end
   end
 
-  describe "GET #de
-  eostroy" do
-    it "returns http success" do
-      get :destroy
+  describe 'DELETE #destroy' do
+    it 'returns http success' do
+      comment = create(:comment)
+      delete :destroy, params: { comment: comment.id }
       expect(response).to have_http_status(:success)
     end
+    it 'recrements the number of posts by 1' do
+      comment = create(:comment)
+      expect do
+        post :destroy, params: { comment: comment.id }
+      end.to change(Post, :count).by(-1)
+    end
   end
-=end
 end
