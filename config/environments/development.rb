eb53c2dc723aa7c962e2286b67aae22c791f8639
khanthulhu <johnlrbrock@gmail.com>
2017-05-03 17:28:50 -0400

    added ssakura for style

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # in production this needs to be set with the production host.
+  # Use letter openener to send emails
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,23 @@
 Rails.application.routes.draw do
-  get '/', to: 'posts#index'
-
-  get 'comments/create'
-
-  get 'comments/destroy'
-
   root 'posts#index'
+  get '/', to: 'posts#index'
+  get 'comments/create'
+  get 'comments/destroy'
 
   match  'auth/:provider/callback',     to: 'sessions#create',                        via: [:get, :post]
   match  'auth/failure',                to: redirect('/'),                            via: :get
   match  'signout',                     to: 'sessions#destroy',        as: 'signout', via: :delete
+
   post   '/friendships/:friend_id',     to: 'friendships#create',      as: 'new_friendship'
   delete '/friendships/:friend_id',     to: 'friendships#destroy',     as: 'destroy_friendship'
   post   '/friend_requests/:friend_id', to: 'friend_requests#create',  as: 'new_friend_request'
   delete '/friend_requests/:friend_id', to: 'friend_requests#destroy', as: 'destroy_friend_request'
   get    '/friend_requests',            to: 'friend_requests#index',   as: 'friend_requests'
-  
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  
+
   resources :users, except: [:new, :create]
   resources :posts, only: [:index, :create]
   resources :comments, only: [:create, :destroy]
   resources :likes, only: [:create, :destroy]
-
 end
